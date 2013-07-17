89b4f4cbd2dc7357c838eebc5c5ddbe41dcbc647
murchik <mixturchik@gmail.com>
2013-07-17 13:00:46 +0400

    Python 3 support edits.

--- sphinxcontrib/httpdomain.py
+++ sphinxcontrib/httpdomain.py
@@ -333,8 +333,8 @@ class HTTPIndex(Index):
     def generate(self, docnames=None):
         content = {}
         items = ((method, path, info)
-            for method, routes in self.domain.routes.iteritems()
-            for path, info in routes.iteritems())
+            for method, routes in self.domain.routes.items()
+            for path, info in routes.items())
         items = sorted(items, key=lambda item: item[1])
         for method, path, info in items:
             entries = content.setdefault(self.grouping_prefix(path), [])
@@ -343,7 +343,7 @@ class HTTPIndex(Index):
                 http_resource_anchor(method, path), '', '', info[1]
             ])
         content = content.items()
-        content.sort(key=lambda (k, v): k)
+        sorted(content, key=lambda k: k[0])
         return (content, True)
 
 
@@ -406,7 +406,7 @@ class HTTPDomain(Domain):
         return dict((key, self.data[key]) for key in self.object_types)
 
     def clear_doc(self, docname):
-        for typ, routes in self.routes.iteritems():
+        for typ, routes in self.routes.items():
             for path, info in routes.items():
                 if info[0] == docname:
                     del routes[path]
@@ -424,8 +424,8 @@ class HTTPDomain(Domain):
                                 contnode, title)
 
     def get_objects(self):
-        for method, routes in self.routes.iteritems():
-            for path, info in routes.iteritems():
+        for method, routes in self.routes.items():
+            for path, info in routes.items():
                 anchor = http_resource_anchor(method, path)
                 yield (path, path, method, info[0], anchor, 1)
 
